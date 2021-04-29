a19dfa30bb0dd3d8fe6460e62d3339ecfe667be5
DavidSchmidt00 <davod2211@gmail.com>
2021-04-29 22:09:46 +0200

    #1: Implemented test-firmware
    Firmware is based on base firmware, but additionaly listens on mqtt messages and makes gpio output.
    Every 10 seconds a alive message is published.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# test-firmware
+# base-firmware
+Test-firmware for ESP32 which implements a test-usecase
+
+Firmware is based on base firmware, but additionaly listens on mqtt messages and makes gpio output.
+Every 10 seconds a alive message is published.
---
+++ src/usecase.hh
@@ -0,0 +1,34 @@
+const int ledPin = 2;
+
+char msg[50];
+int value = 0;
+
+namespace usecase{
+
+    void callback(char* topic, String message) {
+        Serial.print("Message arrived on topic: ");
+        Serial.print(topic);
+        Serial.print(". Message: ");
+
+        if (String(topic) == "command") {
+            /* 
+            Relais:
+            Serial.print("Changing output to ");
+            if(message == "on"){
+                Serial.println("on");
+                digitalWrite(ledPin, HIGH);
+            }
+            else if(message == "off"){
+                Serial.println("off");
+                digitalWrite(ledPin, LOW);
+            } */
+            // My PC-Switch use case ^^
+            if (message=="switch"){
+                Serial.println("Switch!");
+                digitalWrite(ledPin, HIGH);
+                delay(1000);
+                digitalWrite(ledPin, LOW);
+            }
+        }
+    }
+}
